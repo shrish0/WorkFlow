f8c4bd8a1b26d6a1eeb0db063ac92c285e35dbeb
shrish0 <shrishgupta1234@gmail.com>
2024-08-23 12:45:01 +0530

    Untrack appsettings.json and add to .gitignore

--- WorkFlow.Models/obj/Debug/net8.0/WorkFlow.Models.AssemblyInfo.cs
+++ WorkFlow.Models/obj/Debug/net8.0/WorkFlow.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkFlow.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc0ca943bbd8aef390ddf4d32c29b040ab41964e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+263349d3ce05addb6de15b1cc1b86f7e7b256ef3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkFlow.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkFlow.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
